cc114958d2ad92e8e3bf0e5bb06ace1f282f8f60
hoaxisr <avs@rusimp.com>
2026-03-28 09:11:08 +0000

    feat(routing): mount RoutingSearch on routing page

--- frontend/src/lib/components/routing/index.ts
+++ frontend/src/lib/components/routing/index.ts
@@ -1,3 +1,5 @@
 export { default as IpRouteCard } from './IpRouteCard.svelte';
 export { default as IpRouteEditModal } from './IpRouteEditModal.svelte';
 export { default as IpRouteImportModal } from './IpRouteImportModal.svelte';
+export { default as RoutingSearch } from './RoutingSearch.svelte';
+export { default as RoutingSearchResults } from './RoutingSearchResults.svelte';
