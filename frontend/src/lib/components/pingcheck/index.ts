89b09f189c52d2b8f6945ac156dbd252f485174f
hoaxisr <avs@rusimp.com>
2026-03-31 14:26:52 +0000

    feat(pingcheck): create NativeWGPingCheckModal, update exports

--- frontend/src/lib/components/pingcheck/index.ts
+++ frontend/src/lib/components/pingcheck/index.ts
@@ -1,4 +1,5 @@
 export { default as PingCheckStatusCard } from './PingCheckStatusCard.svelte';
 export { default as PingCheckLogsTable } from './PingCheckLogsTable.svelte';
 export { default as NativeWGPingCheckCard } from './NativeWGPingCheckCard.svelte';
+export { default as NativeWGPingCheckModal } from './NativeWGPingCheckModal.svelte';
 export { default as KernelPingCheckModal } from './KernelPingCheckModal.svelte';
