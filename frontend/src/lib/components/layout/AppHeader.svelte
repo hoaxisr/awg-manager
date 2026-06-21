68a345986ee5f94fd3009dd9ee73d05aabba3bc8
hoaxisr <avs@rusimp.com>
2026-06-21 20:05:14 +0000

    fakeip/fe: убрать FAKEIP из navbar (переехал во вкладку)

--- frontend/src/lib/components/layout/AppHeader.svelte
+++ frontend/src/lib/components/layout/AppHeader.svelte
@@ -37,12 +37,6 @@
 			matches: (p) => p.startsWith('/routing'),
 		},
 		{
-			section: 'fakeip',
-			href: '/fakeip',
-			label: 'FAKEIP',
-			matches: (p) => p.startsWith('/fakeip'),
-		},
-		{
 			section: 'monitoring',
 			href: '/monitoring',
 			label: 'МОНИТОРИНГ',
