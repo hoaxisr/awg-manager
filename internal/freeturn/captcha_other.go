adc9458a67ad502e5856c6611188cfbef7ddbfde
hoaxisr <avs@rusimp.com>
2026-07-23 17:35:30 +0000

    freeturn: фикс redeclare в captcha_other.go (сборка вне linux)

--- internal/freeturn/captcha_other.go
+++ internal/freeturn/captcha_other.go
@@ -20,7 +20,3 @@ func socketListenerPIDAmong(host string, port int, _ []int) (int, bool) {
 	_ = conn.Close()
 	return 0, true
 }
-
-func socketListenerPIDAmong(host string, port int, _ []int) (int, bool) {
-	return socketListenerPID(host, port)
-}
