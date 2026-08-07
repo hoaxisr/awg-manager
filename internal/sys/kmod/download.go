2aec6a385cdcb8406ef539c357f2f9631af5a625
hoaxisr <avs@rusimp.com>
2026-08-07 20:53:09 +0000

    kmod: пересобранные модули 3.0.20260805 для 10 представителей

--- internal/sys/kmod/download.go
+++ internal/sys/kmod/download.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	// ExpectedKmodVersion is the kernel module version expected by this build.
-	ExpectedKmodVersion = "3.0.20260731-04"
+	ExpectedKmodVersion = "3.0.20260805"
 
 	// versionFile is the filename that stores the on-disk module version.
 	versionFile = "amneziawg.version"
